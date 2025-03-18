82d6e270badc1e774127211172f6b5c830df0e34
gabrielrp02 <gabrielrp02ok@outlook.com>
2025-03-18 14:54:47 -0400

    added formatter.nvim plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,14 +42,19 @@ return {
         local cmp = require('cmp')
 
         cmp.setup({
-            sources = {
-                {name = 'nvim_lsp'},
-            },
+            sources = cmp.config.sources({
+                                { name = 'nvim_lsp' },
+                            },{  { name = 'buffer' },
+            }),
             snippet = {
                 expand = function(args)
                     -- You need Neovim v0.10 to use vim.snippet
                     vim.snippet.expand(args.body)
                 end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
                 ['<CR>'] = cmp.mapping.confirm({select = true})}),
