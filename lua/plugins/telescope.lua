b50236a505c428efd3c8bdf7865623ad1cf57e09
gabrielrp02 <gabrielrp02ok@outlook.com>
2025-03-23 14:25:04 -0400

    Added keymaps to telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', "<leader>pf", builtin.find_files, {})
-        vim.keymap.set('n', "<leader>ps", function ()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end,{})
-    end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	-- or                              , branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>pg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Telescope buffers" })
+	end,
 }
-
