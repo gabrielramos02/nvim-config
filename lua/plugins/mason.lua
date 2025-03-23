47008cdb63b477ca393667ff445de688a553d63b
gabrielrp02 <gabrielrp02ok@outlook.com>
2025-03-23 14:24:22 -0400

    Added @variable color

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,16 +1,20 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    config = function ()
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            -- Replace the language servers listed here 
-            -- with the ones you want to install
-            ensure_installed = {'lua_ls', 'bashls', 'basedpyright'},
-            handlers = {
-                function(server_name)
-                    require('lspconfig')[server_name].setup({})
-                end,
-            },
-        })
-    end
+	"williamboman/mason-lspconfig.nvim",
+	config = function()
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			-- Replace the language servers listed here
+			-- with the ones you want to install
+			ensure_installed = { "lua_ls", "bashls", "basedpyright" },
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						on_init = function(client, _)
+							client.server_capabilities.semanticTokensProvider = nil
+						end,
+					})
+				end,
+			},
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,8 @@
-return {
-    'nvim-treesitter/nvim-treesitter', cmd = "TSUpdate",
+return { 'nvim-treesitter/nvim-treesitter', cmd = "TSUpdate",
     config = function ()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+            ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
@@ -14,13 +13,13 @@ return {
 
             highlight = {
                 enable = true,
-
                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
                 -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = false,
             },
+            vim.api.nvim_set_hl(0, "@variable", { fg = "#E36397" })
         })
     end
 }
